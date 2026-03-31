91c8849e486854cbfb0c0cc9cc1c96e46fa966da
Dominik Moreira <dominik.privatacc@gmail.com>
2026-03-31 22:52:04 +0200

    add finding alternative words

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -15,6 +15,14 @@ struct App {
         let text = CommandLine.arguments.dropFirst().joined(separator: " ")
         let sentiment = sentiment(for: text)
         print("Sentiment analysis: \(sentiment)")
+
+        let lemma = lemmatize(string: text)
+        print()
+        print("Found the following alternatives:")
+        for word in lemma {
+            let embeddings = embeddings(for: word)
+            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        }
     }
 
     static func sentiment(for string: String) -> Double {
@@ -28,6 +36,46 @@ struct App {
         )
 
         return Double(sentiment?.rawValue ?? "0") ?? 0
+    }
 
+    static func embeddings(for word: String) -> [String] {
+        var results = [String]()
+
+        if let embedding = NLEmbedding.wordEmbedding(for: .english) {
+            let similarWords = embedding.neighbors(for: word, maximumCount: 10)
+
+            for word in similarWords {
+                results.append("\(word.0) has a distance of word \(word.1)")
+            }
+        }
+
+        return results
+    }
+
+    static func lemmatize(string: String) -> [String] {
+        let tagger = NLTagger(tagSchemes: [.lemma])
+        tagger.string = string
+
+        var results = [String]()
+
+        tagger.enumerateTags(
+            in: string.startIndex..<string.endIndex,
+            unit: .word,
+            scheme: .lemma
+        ) {
+            tag,
+            range in
+            let stemForm =
+                tag?.rawValue
+                ?? String(string[range]).trimmingCharacters(in: .whitespaces)
+
+            if stemForm.isEmpty == false {
+                results.append(stemForm)
+            }
+
+            return true
+        }
+
+        return results
     }
 }
