561041f55cb658829450c7ff0049d77ecb8416a8
Dominik Moreira <dominik.privatacc@gmail.com>
2026-04-04 17:10:42 +0200

    detect language and names

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -13,6 +13,11 @@ struct App {
     static func main() {
         print()
         let text = CommandLine.arguments.dropFirst().joined(separator: " ")
+        let language =
+            NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
+        print()
+        print("Detected Language: \(language.rawValue)")
+
         let sentiment = sentiment(for: text)
         print("Sentiment analysis: \(sentiment)")
 
@@ -22,6 +27,13 @@ struct App {
         for word in lemma {
             let embeddings = embeddings(for: word)
             print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        }
+        
+        let entities = entities(for: text)
+        print()
+        print("Found the following entities:")
+        for entity in entities {
+            print("\t", entity)
         }
     }
 
@@ -76,6 +88,31 @@ struct App {
             return true
         }
 
+        return results
+    }
+
+    static func entities(for string: String) -> [String] {
+        let tagger = NLTagger(tagSchemes: [.nameType])
+        tagger.string = string
+        var results = [String]()
+        
+        tagger.enumerateTags(in: string.startIndex..<string.endIndex, unit: .word, scheme: .nameType, options: .joinNames) { tag, range in
+            guard let tag = tag else { return true }
+
+            let match = String(string[range])
+
+            switch tag {
+            case .organizationName:
+                results.append("Organization: \(match)")
+            case .personalName:
+                results.append("Person: \(match)")
+            case .placeName:
+                results.append("Place: \(match)")
+            default:
+                break
+            }
+            return true
+        }
         return results
     }
 }
