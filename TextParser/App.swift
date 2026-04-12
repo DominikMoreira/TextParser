3976294b3afac07e8d366f43062f5d82c0200f60
Dominik Moreira <dominik.privatacc@gmail.com>
2026-04-12 22:23:13 +0200

    add flags and options

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -11,33 +11,98 @@ import NaturalLanguage
 
 @main
 struct App: ParsableCommand {
+    static var configuration: CommandConfiguration {
+        CommandConfiguration(
+            commandName: "analyze",
+            abstract:
+                "Analyzes input text using a range of natural language approaches."
+        )
+    }
+
     @Argument(help: "The text you want to analyze")
     var input: [String]
-    
+
+    @Flag(name: .shortAndLong, help: "Show detected language.")
+    var detectLanguage = false
+
+    @Flag(
+        name: .shortAndLong,
+        help: "Prints how positive or negative the input is."
+    )
+    var sentimentAnalysis = false
+
+    @Flag(
+        name: .shortAndLong,
+        help: "Shows the stem form of each word in the input."
+    )
+    var lemmatize = false
+
+    @Flag(
+        name: .shortAndLong,
+        help: "Prints alternative words for each word in the input."
+    )
+    var alternatives = false
+
+    @Flag(
+        name: .shortAndLong,
+        help: "Prints names of people, places, and organizations in the input."
+    )
+    var names = false
+
+    @Option(help: "The maximum number of alternatives to suggest.")
+    var maximumAlternatives = 10
+
     mutating func run() {
+        if detectLanguage == false && sentimentAnalysis == false
+            && lemmatize == false && alternatives == false && names == false
+        {
+            detectLanguage = true
+            sentimentAnalysis = true
+            lemmatize = true
+            alternatives = true
+            names = true
+        }
+
         print()
         let text = input.joined(separator: " ")
-        let language =
-            NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
-        print()
-        print("Detected Language: \(language.rawValue)")
 
-        let sentiment = sentiment(for: text)
-        print("Sentiment analysis: \(sentiment)")
-
-        let lemma = lemmatize(string: text)
-        print()
-        print("Found the following alternatives:")
-        for word in lemma {
-            let embeddings = embeddings(for: word)
-            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        if detectLanguage {
+            let language =
+                NLLanguageRecognizer.dominantLanguage(for: text)
+                ?? .undetermined
+            print()
+            print("Detected Language: \(language.rawValue)")
         }
-        
-        let entities = entities(for: text)
-        print()
-        print("Found the following entities:")
-        for entity in entities {
-            print("\t", entity)
+
+        if sentimentAnalysis {
+            let sentiment = sentiment(for: text)
+            print("Sentiment analysis: \(sentiment)")
+        }
+
+        lazy var lemma = lemmatize(string: text)
+        if lemmatize {
+            print()
+            print("Found the following lemma:")
+            print("\t", lemma.formatted(.list(type: .and)))
+        }
+
+        if alternatives {
+            print()
+            print("Found the following alternatives:")
+
+            for word in lemma {
+                let embeddings = embeddings(for: word)
+                print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+            }
+        }
+
+        if names {
+            let entities = entities(for: text)
+            print()
+            print("Found the following entities:")
+            for entity in entities {
+                print("\t", entity)
+            }
         }
     }
 
@@ -58,7 +123,10 @@ struct App: ParsableCommand {
         var results = [String]()
 
         if let embedding = NLEmbedding.wordEmbedding(for: .english) {
-            let similarWords = embedding.neighbors(for: word, maximumCount: 10)
+            let similarWords = embedding.neighbors(
+                for: word,
+                maximumCount: maximumAlternatives
+            )
 
             for word in similarWords {
                 results.append("\(word.0) has a distance of word \(word.1)")
@@ -99,8 +167,13 @@ struct App: ParsableCommand {
         let tagger = NLTagger(tagSchemes: [.nameType])
         tagger.string = string
         var results = [String]()
-        
-        tagger.enumerateTags(in: string.startIndex..<string.endIndex, unit: .word, scheme: .nameType, options: .joinNames) { tag, range in
+
+        tagger.enumerateTags(
+            in: string.startIndex..<string.endIndex,
+            unit: .word,
+            scheme: .nameType,
+            options: .joinNames
+        ) { tag, range in
             guard let tag = tag else { return true }
 
             let match = String(string[range])
